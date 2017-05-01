786c30696ce90ea68b0b49668b78f8f5842bf1d0
Cupprum <branisa.samuel@windowslive.com>
2017-05-01 21:30:46 +0200

    otazky vybera z databazy

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, render_template, make_response, session
 import random
-import xml.etree.ElementTree as ET
 import json
 import uuid
 import psycopg2
@@ -15,39 +14,11 @@ conn = psycopg2.connect(db)
 conn.autocommit = True
 engine = conn.cursor()
 engine.execute("CREATE TABLE IF NOT EXISTS FIIT (uuia4 text, meno text, body int, stav text);")
-engine.execute("CREATE TABLE IF NOT EXISTS otazky (cislootazky int, ot text, od text, ma text, mb text, mc text, md text, me text, mf text, mg text, mh text);")
 
-tree = ET.parse('chemia.xml')
-root = tree.getroot()
 moja = []
 ID = []
 otazka = []
 uvod = 'Zvol spravne odpovede. Skontroluj svoje odpovede kliknutim na tlacitko kontrola.'
-
-ypsilon = 1
-for otazky in root.findall('otazka'):
-    number = otazky.attrib.get('number')
-    if str(ypsilon) == number:
-        print('ypsilon: ', ypsilon, 'number: ', number)
-        ot = str(otazky.find('ot').text)
-        od = str(otazky.find('od').text)
-        ma = str(otazky.find('ma').text)
-        mb = str(otazky.find('mb').text)
-        mc = str(otazky.find('mc').text)
-        md = str(otazky.find('md').text)
-        me = str(otazky.find('me').text)
-        mf = str(otazky.find('mf').text)
-        mg = str(otazky.find('mg').text)
-        mh = str(otazky.find('mh').text)
-        vklada = """INSERT INTO otazky (cislootazky, ot, od, ma, mb, mc, md, me, mf, mg, mh) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
-        engine.execute(vklada, (ypsilon, ot, od, ma, mb, mc, md, me, mf, mg, mh))
-        ypsilon += 1
-
-engine.execute("""SELECT * FROM otazky""")
-result_set = engine.fetchall()
-for r in result_set:
-    print(r)
-
 
 def kont():
     A = request.form.get('A')
