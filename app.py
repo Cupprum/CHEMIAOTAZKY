ca567d05d295e0028fa48eed639bd214ce846006
Cupprum <branisa.samuel@windowslive.com>
2018-04-26 22:02:29 +0200

    had problem with reloading

--- app.py
+++ app.py
@@ -438,4 +438,6 @@ def wrong_answered():
 
 
 if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True, use_reloader=True)
